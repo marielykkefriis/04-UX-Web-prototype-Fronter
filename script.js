4c512a179a678bb7e594b7f1362696ad61b7d0c9
Marie Friis Larsen <Mariefriis@Maries-MacBook-Pro.local>
2019-05-29 12:33:49 +0200

    Færdig

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+        document.querySelector("section").classList = "";
+    } else {
+        document.querySelector("#menuknap").textContent = "x";
+        document.querySelector("section").classList = "hidden";
+    }
+}
